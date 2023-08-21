3c6b82c28d8728cfd67e93ce907552fe9899575e
JR-Fank <62536004+JR-Fank@users.noreply.github.com>
2023-08-22 01:51:31 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      <div className={styles.copyright}>JR-Fank 2019 {config.author}</div> 
 
       <div className={styles.settings}>
         {hasMounted && (
@@ -68,7 +68,7 @@ export const FooterImpl: React.FC = () => {
           <a
             className={styles.mastodon}
             href={config.mastodon}
-            title={`Mastodon ${config.getMastodonHandle()}`}
+            title={`Mastodon ${config.getMastodonHandle()}`} 
             rel='me'
           >
             <FaMastodon />
