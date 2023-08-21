2792eb92924c042c7ff7d0da9e7d70d62515b162
JR-Fank <62536004+JR-Fank@users.noreply.github.com>
2023-08-22 02:13:21 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,7 +19,7 @@ import styles from './styles.module.css'
 
 export const FooterImpl: React.FC = () => {
   const [hasMounted, setHasMounted] = React.useState(false)
-  const { isDarkMode, toggleDarkMode } = useDarkMode()
+  const { isDarkMode, toggleDarkMode } = useDarkMode() 
 
   const onToggleDarkMode = React.useCallback(
     (e) => {
@@ -35,7 +35,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>JRFank 2019 {config.author}</div>  
+      <div className={styles.copyright}>Copyright 2022 {config.author}</div>   
 
       <div className={styles.settings}>
         {hasMounted && (
